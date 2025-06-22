16fd14117262905e6922323b6aa8ea2099cb1f61
Pavel Demin <paveldemin@me.com>
2025-06-22 22:56:02 +0100

    [root] refactor: pass env variables to shinyapp

--- shinyproxy/app/R/cache.R
+++ shinyproxy/app/R/cache.R
@@ -3,7 +3,7 @@ mock_redis_client <- list(GET = function(...) NULL, SET = function(...) NULL)
 get_redis_client <- function() {
   config <- list(host = redis_host, port = redis_port)
   if (redux::redis_available(config)) {
-    redux::hiredis(config = list(host = redis_host, port = redis_port))
+    redux::hiredis(config = config)
   } else {
     warning("Cache is not available.", call. = FALSE)
     mock_redis_client
